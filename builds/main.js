509d6e91575721f4b0c27038443994cefc1be5ae
Garrett <graffitisouls@gmail.com>
2016-06-28 07:59:54 -0400

    Posting should work now.

--- builds/main.js
+++ builds/main.js
@@ -1,14 +1,45 @@
 'use strict';
 
+require('dotenv').config();
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(bodyParser.json());
+
+//pg setup
+var pg = require('pg');
+var db_config = {
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  max: 10,
+  idleTimeoutMillis: 30000
+};
+var pool = new pg.Pool(db_config);
+
+//Business code.
+app.get('/score/:id', function (req, res) {});
+app.post('/score', function (req, res) {
+  pool.connect(function (err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool.');
+    }
+    var _req$body = req.body;
+    var score = _req$body.score;
+    var time = _req$body.time;
+    var initials = _req$body.initials;
+    var grade = _req$body.grade;
+
+    client.query('INSERT INTO runs(score,time,initials,grade) VALUES($1,$2,$3,$4)', [score, time, initials, grade], function (err, result) {
+      done();
+      console.log(err);
+      console.log(result);
+    });
+  });
 });
 
-app.get('/score/:id', function (req, res) {});
-app.post('/score', function (req, res) {});
 app.get('/scores', function (req, res) {});
 
 app.listen(3111, function () {
--- src/main.js
+++ src/main.js
@@ -1,27 +1,37 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+
+//pg setup
 const pg = require('pg');
 const db_config = {
   port: process.env.PGPORT,
   database: process.env.PGDATABASE,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
-  max: 10
+  max: 10,
   idleTimeoutMillis: 30000
 }
 var pool = new pg.Pool(db_config);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-//The whole app!
+//Business code.
 app.get('/score/:id', (req, res) => {});
 app.post('/score', (req,res) => {
-
-
+  pool.connect((err, client, done) => {
+    if (err) {
+      return console.error('error fetching client from pool.');
+    }
+    const {score, time, initials, grade} = req.body;
+    client.query('INSERT INTO runs(score,time,initials,grade) VALUES($1,$2,$3,$4)'
+                 ,[score, time, initials, grade], (err, result) => {
+      done();
+      console.log(err);
+      console.log(result);
+    });
+  })
 });
 
 app.get('/scores', (req, res) => {});
