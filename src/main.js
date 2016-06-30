611155bdfb63a7b5bcbedf7a593ee7d5274de6ed
Garrett <graffitisouls@gmail.com>
2016-06-30 18:15:25 -0400

    Alright this is just annoying for no value. Ripping out PG for something simpler.

--- src/main.js
+++ src/main.js
@@ -1,55 +1,19 @@
-if (process.env.NODE_ENV == "dev") {require('dotenv').config();}
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-//pg setup
-const pg = require('pg');
-const db_config = {
-  host: process.env.DATABASE_URL,
-  ssl: process.env.USE_SSL,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  max: 10,
-  idleTimeoutMillis: 30000
-}
-var pool = new pg.Pool(db_config);
-
 //Business code.
 app.post('/', (req,res) => {
   const {score, time, initials, grade} = req.body;
-  pool.query('INSERT INTO runs(score,time,initials,grade) VALUES($1,$2,$3,$4)'
-               ,[score, time, initials, grade], (err, result) => {
-    console.log(err);
-    console.log(result);
-  });
+  //'INSERT INTO runs(score,time,initials,grade) VALUES($1,$2,$3,$4)',[score, time, initials, grade]
 });
 
 app.get('/', (req, res) => {
-  console.log('get received.')
-  pool.query("(SELECT initials, TO_CHAR(time, 'MI:SS.MS'), score, grade FROM runs WHERE grade = 'GM' ORDER BY time) UNION ALL (SELECT initials, TO_CHAR(time, 'MI:SS.MS'), score, grade FROM runs WHERE grade != 'GM' ORDER BY score DESC)",
-  (err, result) => {
-    console.log('processing query.');
-    if(err) {
-      console.log(err)
-    } else {
-      console.log('responding...');
-      res.json({list: result.rows})
-    }
-  });
+  //"(SELECT initials, TO_CHAR(time, 'MI:SS.MS'), score, grade FROM runs WHERE grade = 'GM' ORDER BY time) UNION ALL (SELECT initials, TO_CHAR(time, 'MI:SS.MS'), score, grade FROM runs WHERE grade != 'GM' ORDER BY score DESC)",
+  res.json({list: result})
 });
-
-app.get('/test',(req, res) => {
-  console.log('test!');
-  console.log(pool);
-  pool.query("SELECT * FROM runs", (err, result) => {
-    console.log("queried.")
-  })
-})
 
 app.listen(process.env.PORT || 3111, function () {
   console.log('Leaderboards active!');
